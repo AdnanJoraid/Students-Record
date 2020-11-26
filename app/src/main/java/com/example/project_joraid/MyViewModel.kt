e9a442eb74cba88850ccbe220d2f1b3dbea49e08
Adnan Joraid <adnanjoraidca@gmail.com>
2020-11-26 16:57:07 -0500

    Implemented my viewmodel class

--- app/src/main/java/com/example/project_joraid/MyViewModel.kt
+++ app/src/main/java/com/example/project_joraid/MyViewModel.kt
@@ -1,4 +1,68 @@
 package com.example.project_joraid
 
-class MyViewModel {
+import android.app.Application
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.BufferedReader
+import java.io.InputStream
+import org.json.JSONObject
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.coroutines.CoroutineContext
+import kotlin.math.log
+
+class MyViewModel(app : Application) : AndroidViewModel(app) {
+
+    var loginIsValid = MutableLiveData<String>()
+
+    suspend fun loadData(url: String): String{
+        var ins: InputStream? = null
+        var result = ""
+
+        try{
+            var url = URL(url)
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "POST"
+            ins = connection.inputStream
+            result = ins.bufferedReader().use(BufferedReader::readText)
+
+        } catch (e: Exception){
+            Log.d("Json error-> ", e.toString())
+        }
+
+        return result
+    }
+
+    fun makeRequest(url: String){
+        CoroutineScope(Dispatchers.IO).launch {
+            var result = loadData(url)
+
+            withContext(Dispatchers.Main){
+                var json = getJson(result)
+
+                var isValidLogin = json.optString("login")
+
+                updateInfo(isValidLogin)
+                Toast.makeText(getApplication(), isValidLogin, Toast.LENGTH_LONG).show()
+
+
+            }
+        }
+    }
+
+    fun updateInfo(login:String){
+        this.loginIsValid.value = login
+    }
+
+    fun getJson(jsonStr: String): JSONObject{
+        var json = JSONObject(jsonStr)
+        return json
+    }
 }
