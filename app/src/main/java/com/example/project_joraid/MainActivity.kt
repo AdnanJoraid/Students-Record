50ef690c3b450317b21f807beb0e2ae37c99b179
Adnan Joraid <adnanjoraidca@gmail.com>
2020-11-26 15:46:16 -0500

    Worked on the UI of the login in screen

--- app/src/main/java/com/example/project_joraid/MainActivity.kt
+++ app/src/main/java/com/example/project_joraid/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.project_joraid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     var  x = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun loginBtnEvent(view: View) {
+        //all the login functionally will be inside this function
     }
 }
