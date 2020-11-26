42854e8ca91f6a195e7a92c8851e0febae276472
Adnan Joraid <adnanjoraidca@gmail.com>
2020-11-26 16:30:22 -0500

    Worked on the UI of the login in screen and created the view model class for retrieving the login data from the server

--- app/src/main/java/com/example/project_joraid/MainActivity.kt
+++ app/src/main/java/com/example/project_joraid/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.project_joraid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var  x = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,5 +14,15 @@ class MainActivity : AppCompatActivity() {
 
     fun loginBtnEvent(view: View) {
         //all the login functionally will be inside this function
+
+        var username = etUserName.text.toString()
+        var password = etPassword.text.toString()
+        var rememberMe = cbRememberMe.isChecked.toString()
+
+       Toast.makeText(this, rememberMe, Toast.LENGTH_LONG).show()
+
+        /*
+        1-
+         */
     }
 }
